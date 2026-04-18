5e70214bd96280e0a088c1562b566ec40b2b50cd
nikitamaratovz-maker <nikitamaratovz@gmail.com>
2026-04-18 17:58:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+BOT_TOKEN = "8705552809:AAF54jFP1wMyA-E4GQinBRWRKHnoNTS-Ak4"
+OPENROUTER_API_KEY = "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"
+OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
+MODEL = "deepseek/deepseek-chat"
+
+REQUIRED_CHANNEL_ID = -1003766526712
+REQUIRED_CHANNEL_LINK = "https://t.me/krectbll"
+
+TRIGGER_WORD = "зара"  # слово для активации в чатах
